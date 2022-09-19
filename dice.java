a7fe3dc1b5ce30c3abb5acbf1768498f39a4d5da
claytonCarter <cgc1101ri@gmail.com>
2022-09-18 21:09:30 -0400

    data gen
    dice sim complete

--- dice.java
+++ dice.java
@@ -4,12 +4,12 @@ public class dice{
     Random roll = new Random();
     int[] rollTracker = new int[6];
 
-    public int roll(int rolls){
+    public int[] roll(int rolls){
         int result;
-        for(int i=1;i<=rolls;i++){
+        for(int i=0;i<rolls;i++){
             result = roll.nextInt(10);
-            if(result<=5){
-                rollTracker[result-1]++;
+            if(result<5){
+                rollTracker[result]++;
             }
             else{
                 rollTracker[5]++;
--- sim.java
+++ sim.java
@@ -1,9 +1,51 @@
-import dice; 
+import java.util.Arrays;
+import java.io.File;  // Import the File class
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.util.HashMap;
+
 public class sim{
+    private int[] results;
+    
+    private void CreateFile() {
+  
+        try {
+            File myObj = new File("simulatedData.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    
+    private void writeFile(String results){
+          try{
+            FileWriter myWriter = new FileWriter("simulatedData.txt");
+            myWriter.write(results);
+            myWriter.close();
+        }catch(IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args){
-        dice test = dice();
-        int results = test.roll(20);
+        sim mySim = new sim();
+        mySim.CreateFile();
+        String simRecords = ""; 
+        int[] dataCount = {20 , 200 , 2000};
+        dice test = new dice();
+
+        for(int num:dataCount){
+            int[] results = test.roll(num);
+            simRecords =simRecords + Arrays.toString(results);  
+            System.out.println(Arrays.toString(results));
+        }
+
+        mySim.writeFile(simRecords);
         
-        System.out.println(results);
 }}
-// set path=%path%;C:\Program javaFiles\Java\jdk-15\bin
